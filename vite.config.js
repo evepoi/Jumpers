9ef3815d42ff501143c1204e0fb0dbfd982bd9e5
evepoi <evepoi@naver.com>
2026-02-02 21:41:32 +0900

    fix: force base for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-export default defineConfig(({ command }) => {
-  // dev 서버는 루트(/)로, GitHub Pages 빌드(prod)만 /Jumpers/로
-  const isBuild = command === "build";
-  return {
-    plugins: [vue()],
-    base: isBuild ? "/Jumpers/" : "/",
-  };
+export default defineConfig({
+  plugins: [vue()],
+  base: "/Jumpers/",
 });
