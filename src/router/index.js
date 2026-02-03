4bd90ac0beb8f15accbdc30f1d45f52874d493ff
evepoi <evepoi@naver.com>
2026-02-04 00:34:32 +0900

    배포 오류 수정

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,14 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
+
+// 예시 컴포넌트
+import HomeView from "@/views/HomeView.vue";
+import SettingsView from "@/views/SettingsView.vue";
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL), // ✅ /Jumpers/ 자동 반영
   routes: [
-    // { path: "/", component: Home },
-    // { path: "/settings", component: Settings },
+    { path: "/", name: "home", component: HomeView },
+    { path: "/settings", name: "settings", component: SettingsView },
   ],
 });
 
